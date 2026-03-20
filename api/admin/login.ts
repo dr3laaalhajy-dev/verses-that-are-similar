49135c2737c2b8bbdd4a608b6779a8d2fabbd339
Apple <apple@UCs-MacBook-Air.local>
2026-03-20 07:04:03 +0200

    feat: add

--- api/admin/login.ts
+++ api/admin/login.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { prisma } from '../_lib/prisma'
+import { prisma } from '../_lib/prisma.js'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
